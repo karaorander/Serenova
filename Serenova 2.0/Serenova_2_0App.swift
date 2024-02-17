350d530753ec7a2a26b6836b9fd03043367948e9
Cristina Corley <corley6@purdue.edu>
2024-02-17 15:52:52 -0500

    Linked Firebase

--- Serenova 2.0/Serenova_2_0App.swift
+++ Serenova 2.0/Serenova_2_0App.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseDatabase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+
+}
+
+
 
 @main
 struct Serenova_2_0App: App {
+    // Register App Delegate for Firebase Setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
