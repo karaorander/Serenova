4a7671309a656767ddb4a356e5bc029c41c11a5e
Ishwarya <isamaved@purdue.edu>
2024-02-20 15:30:53 -0500

    Marys Commit

---
+++ Serenova 2.0/QuestionareView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionareView.swift
+//  Serenova 2.0
+//
+//  Created by Ishwarya Samavedhi on 2/19/24.
+//
+
+import SwiftUI
+
+struct QuestionareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionareView()
+}
