0b5de868a7b4c606ec044ad6dc6a7e0c4c7e3fc6
Mary Voorhees <mvoorhe@purdue.edu>
2024-02-22 14:29:04 -0800

    Beginnings for sleep goal being stored to Firebase (commented out).

--- Serenova 2.0/ContentView.swift
+++ Serenova 2.0/ContentView.swift
@@ -6,11 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
+import UIKit
 
 struct ContentView: View {
+    // private let database = Database.database().reference()
+
     var body: some View {
         LoginView()
     }
+
+    // *in the form of an object for sake of readability in database
+    // *currently 0 as placeholder until user input
+    // let goal: [String: int] = [
+    //      "Sleep Hours": 0
+    //  ]
+    // database.child("Sleep Goals").setValue(goal)
 }
 
 #Preview {
