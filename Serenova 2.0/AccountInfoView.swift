5df699101604dc6cc8059bbc3042648c358e06b7
karaorander <korande@purdue.edu>
2024-02-26 15:25:51 -0500

    Added New Edit Profile UI page under the AccountInfoView page

--- Serenova 2.0/AccountInfoView.swift
+++ Serenova 2.0/AccountInfoView.swift
@@ -46,6 +46,7 @@ struct AccountInfoView: View {
                 ZStack{
                     LinearGradient(gradient: Gradient(colors: [.dreamyTwilightMidnightBlue.opacity(0.2), .nightfallHarmonyNavyBlue.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
                         .ignoresSafeArea()
+                    
                     VStack {
                         Text("Account Info")
                             .font(Font.custom("NovaSquare-Bold", size: 40))
@@ -56,7 +57,8 @@ struct AccountInfoView: View {
                         
                         // Color drop down menu
                         // Color theme stored in $color_theme
-                        VStack {
+                        /*VStack {
+                            
                             HStack {
                                 Button {
                                     color_theme = "Moonlit Serenity"
@@ -96,7 +98,7 @@ struct AccountInfoView: View {
                             }
                             
                             Spacer().frame(height:30)
-                        }
+                        }*/
                         
                         Image(.userimageprofile)
                             .resizable()
@@ -165,43 +167,56 @@ struct AccountInfoView: View {
                             }
                             .font(.system(size: 17)).fontWeight(.medium).frame(width: 300, height: 40, alignment: .leading).background(Color.tranquilMistAshGray).foregroundColor(.nightfallHarmonyNavyBlue).cornerRadius(5)
                         }
+                        
+                        // Edit Profile
+                        NavigationLink(destination: EditProfileView()){
+                            HStack{
+                                Text("Edit Profile")
+                                    .padding()
+                                Spacer()
+                                Image(systemName:
+                                        "arrow.right")
+                                .padding()
+                            }
+                            .font(.system(size: 17)).fontWeight(.medium).frame(width: 300, height: 40, alignment: .leading).background(Color.tranquilMistAshGray).foregroundColor(.nightfallHarmonyNavyBlue).cornerRadius(5)
+                        }
                     }
                     Spacer()
                     
-                //Menu Bar
+                    //Menu Bar
                 }.overlay(alignment: .bottom, content: {
                     
                     HStack (spacing: 40){
                         
                         NavigationLink(destination: SleepLogView().navigationBarBackButtonHidden(true)) {
-
+                            
                             Image(systemName: "zzz")
                                 .resizable()
                                 .frame(width: 30, height: 30)
                                 .foregroundColor(.white)
-                        
-                    }
+                            
+                        }
                         NavigationLink(destination: SleepGoalsView().navigationBarBackButtonHidden(true)) {
-
+                            
                             Image(systemName: "list.clipboard")
                                 .resizable()
                                 .frame(width: 30, height: 40)
                                 .foregroundColor(.white)
-                        
-                    }
-                        NavigationLink(destination: ForumPostView().navigationBarBackButtonHidden(true)) {
-
-                                Image(systemName: "person.2")
-                                    .resizable()
-                                    .frame(width: 45, height: 30)
-                                    .foregroundColor(.white)
                             
                         }
-                }.padding()
-                .hSpacing(.center)
-                .background(Color.dreamyTwilightMidnightBlue)
-                
-            })
+                        NavigationLink(destination: ForumPostView().navigationBarBackButtonHidden(true)) {
+                            
+                            Image(systemName: "person.2")
+                                .resizable()
+                                .frame(width: 45, height: 30)
+                                .foregroundColor(.white)
+                            
+                        }
+                    }.padding()
+                        .hSpacing(.center)
+                        .background(Color.dreamyTwilightMidnightBlue)
+                    
+                })
                 
             }.buttonStyle(PlainButtonStyle())
             
@@ -262,6 +277,54 @@ struct BioInfoView: View {
                         }
                         // TODO: Store night owl early bird preference
                     }
+                }
+                
+            }
+        }
+    }
+}
+struct EditProfileView: View {
+    // TODO: Get username, full name, email, notification preferences from database
+    @State public var myusername = "MYUSERNAME"
+    @State public var myemail = "MYEMAIL"
+    @State private var fullname = "MYNAME"
+    var body: some View {
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.dreamyTwilightMidnightBlue.opacity(0.2), .nightfallHarmonyNavyBlue.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack {
+                        Text("Edit info")
+                        .font(Font.custom("NovaSquare-Bold", size: 40))
+                            .frame(height: 2.0, alignment: .leading)
+                            .padding()
+                    
+                    // Submit Bio button
+                    Button ("Submit", action: {
+                        // TODO: Store BIO in database
+                    })
+                    .font(.system(size: 20)).fontWeight(.medium).frame(width: 300, height: 50).background(Color.soothingNightLightGray.opacity(0.6)).foregroundColor(.nightfallHarmonyNavyBlue.opacity(1)).cornerRadius(10)
+                    
+                    Spacer().frame(height: 60)
+                    // Night Owl or Early Bird Question
+                    Text("Night Owl or Early Bird?")
+                        .font(Font.custom("NovaSquare-Bold", size: 25))
+                    Spacer().frame(height: 30)
+                    
+                    /*LazyVGrid(columns: preferenceColumns, spacing: 20) {
+                        ForEach(data, id: \.self) { option in
+                            Button(action: {
+                                self.sleepPreference = option == self.sleepPreference ? "Early Bird" : option
+                            }) {
+                                Text(option)
+                                    .padding().frame(width: 150, height: 50).foregroundColor(.white)
+                            }
+                            
+                            .background(self.sleepPreference == option ? Color.soothingNightLightGray.opacity(0.5) : Color.nightfallHarmonyRoyalPurple.opacity(0.8))
+                            .cornerRadius(10)
+                        }
+                        // TODO: Store night owl early bird preference
+                    }*/
                 }
                 
             }
