ee585ef3006b6c9c5b76205ce1a266af8cd33319
karaorander <korande@purdue.edu>
2024-02-27 16:28:02 -0500

    Created Sleep Graph UI

---
+++ Serenova 2.0/SleepGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  SleepGraphView.swift
+//  Serenova 2.0
+//
+//  Created by Kara Orander on 2/27/24.
+//
+
+import SwiftUI
+
+struct SleepGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SleepGraphView()
+}
