17bd13f708b560939cd485484f9b43e6970da213
Ishwarya <isamaved@purdue.edu>
2024-02-27 18:42:26 -0500

    AlarmView and List of Alarm UI's Created - Not linked yet, just using dummy values to test, however, edit + delete button  in List of Alarm UI works.

---
+++ Serenova 2.0/AlarmListView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmListView.swift
+//  Serenova 2.0
+//
+//  Created by Ishwarya Samavedhi on 2/27/24.
+//
+
+import SwiftUI
+
+struct AlarmListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmListView()
+}
