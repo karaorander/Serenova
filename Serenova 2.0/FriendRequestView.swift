be3e971d1bf674ea041e2b6f91cfd008dd86f721
karaorander <korande@purdue.edu>
2024-03-26 16:40:15 -0400

    Fixed nil error bug in rewardsDashboardView()

--- Serenova 2.0/FriendRequestView.swift
+++ Serenova 2.0/FriendRequestView.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import FirebaseStorage
 
 struct FriendRequestView: View {
     var body: some View {
@@ -15,6 +19,28 @@ struct FriendRequestView: View {
                 LinearGradient(gradient: Gradient(colors: [.nightfallHarmonySilverGray.opacity(0.7), .nightfallHarmonyRoyalPurple.opacity(0.3), .dreamyTwilightMidnightBlue.opacity(0.5), .nightfallHarmonyNavyBlue.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
                     .ignoresSafeArea()
             }
+        }
+    }
+    
+    func queryRequests(NUM_REQUESTS: Int) async {
+        // Database Reference
+    }
+}
+
+struct NoRequestsView: View {
+    var body: some View {
+        VStack (alignment: .center){
+            Spacer()
+            Image(systemName: "moon.stars.fill")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .foregroundColor(.white)
+                .padding()
+            Text("No Posts Yet")
+                .foregroundColor(.white)
+                .font(.system(size: 30))
+                .fontWeight(.semibold)
+            Spacer()
         }
     }
 }
