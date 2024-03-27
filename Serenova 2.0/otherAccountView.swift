47bf4fe0fdbcaa4613880fad4fcf81c4bfcadfc3
Ishwarya <isamaved@purdue.edu>
2024-03-27 18:27:54 -0400

    Added Other Account View (must take from finder and drag the file in,  pulling wont give you the file since its new); shows account info based on userID

---
+++ Serenova 2.0/otherAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  otherAccountView.swift
+//  Serenova 2.0
+//
+//  Created by Ishwarya Samavedhi on 3/27/24.
+//
+
+import SwiftUI
+
+struct otherAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    otherAccountView()
+}
