b96d9642f91dc2770f8e5df82d745db8e708f184
Caitlin Wilson <wils1353@purdue.edu>
2024-04-10 19:12:36 -0400

    Update Conversation Class Functions

--- Serenova 2.0/Class/Conversation.swift
+++ Serenova 2.0/Class/Conversation.swift
@@ -1,6 +1,30 @@
+//
+//  Conversation.swift
+//  Serenova 2.0
+//
+//  Created by Caitlin Wilson on 2/19/24.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseDatabase
+
+// Global User Variable
+var currConvo: Conversation?
+
+
 class Conversation {
-    /*var participantOne
-    var ParticipantTwo
-    var messages = []
-    boolean convoid = false*/
+    //var participantOne: User?
+    //var participantTwo: User?
+    public var participants: [String] = []
+    public var messages: [Message] = []
+    public var convoId: String = ""
+    
+    init() {}
+
+    func addMessage(_ messageToAdd: Message) {
+        messages.append(messageToAdd)
+    }
 }
